3b29f77ebea6dcd49380beb4cef35cf798eb7a61
amy <ctrlalternativedelete@gmail.com>
2022-10-20 17:45:10 +1100

    fixed up

--- src/store/peer.ts
+++ src/store/peer.ts
@@ -1,5 +1,5 @@
-import { OnPeerDevicesMessage } from "modules/peer/data";
-import { PeerDevicesMessage } from "modules/peer/message";
+import { getDevicePtr, OnPeerDevicesMessage } from "modules/peer/data";
+import { PeerDevicesMessage, PeerMessage } from "modules/peer/message";
 import { JSONTools } from "modules/peer/tools";
 import { Peer as TPeer } from "peerjs";
 import create from "zustand";
@@ -17,10 +17,23 @@ const usePeerStore = create<PeerStoreState>((set, get) => ({
         const { Peer } = await import('peerjs');
         const peer = new Peer();
         peer?.on("connection", (c) => {
-            c.on("data", (d) => {
-                OnPeerDevicesMessage(d as PeerDevicesMessage, c);
+            c.on("data", (data) => {
+                const d = data as PeerMessage;
                 const { devices } = useButtplugStore.getState();
-                c.send({ type: "devices", devices: JSONTools.strip(devices) } as PeerDevicesMessage)
+                if (d.type === "devices") {
+                    OnPeerDevicesMessage(d as PeerDevicesMessage, c);
+                    c.send({ type: "devices", devices: JSONTools.strip(devices) } as PeerDevicesMessage)
+                }
+                if (d.type === "method") {
+                    const device_index = devices.findIndex(e => getDevicePtr(e) === d.devicePtr);
+                    if (device_index !== -1) {
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        const args: unknown[] = (d as any).params || [];
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        (devices[device_index] as any)[d.method](...args);
+                    }
+                    return;
+                }
             })
         })
         set(() => ({peer}));
