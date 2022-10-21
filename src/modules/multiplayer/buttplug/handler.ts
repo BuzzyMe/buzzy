2655afaf07c32e5f75a6b3888b00ff3205a0b5af
amy <ctrlalternativedelete@gmail.com>
2022-10-21 15:57:43 +1100

    updated handler

--- src/modules/multiplayer/buttplug/handler.ts
+++ src/modules/multiplayer/buttplug/handler.ts
@@ -1,32 +1,31 @@
 import { ButtplugClient, ButtplugClientDevice } from "buttplug";
-import { DataConnection, MediaConnection } from "peerjs";
+import { DataConnection, MediaConnection, Peer } from "peerjs";
 import useButtplugStore from "store/buttplug";
 import usePeerStore from "store/peer";
 import { PeerDevice } from "../peer/device";
 import { PeerDevicesMessage } from "../peer/message";
 import { JSONTools } from "../peer/tools";
 
-const handler = async (client: ButtplugClient) => {
-    
+const buttplug_handler = async (client: ButtplugClient) => {
     const send_devices = (devices: (PeerDevice | ButtplugClientDevice)[]) => {
         const { peer } = usePeerStore.getState();
         const connections: (DataConnection | MediaConnection)[] | undefined = peer?.connections ? Object.values(peer?.connections).flatMap(e => e) : undefined;
         if (!connections) return;
         for (const c of connections) {
-            if (c instanceof DataConnection) {
-                c.send({ type: "devices", devices: JSONTools.strip(devices) } as PeerDevicesMessage)
+            if (typeof (c as any).send === "function") {
+                (c as DataConnection).send({ type: "devices", devices: JSONTools.strip(devices) } as PeerDevicesMessage)
             }
         }
     }
 
-    client.on("deviceadded", (d) => {
+    client?.on("deviceadded", (d) => {
         const { devices } = useButtplugStore.getState();
         send_devices([d, ...devices])
     })
-    client.on("deviceremoved", (d) => {
+    client?.on("deviceremoved", (e) => {
         const { devices } = useButtplugStore.getState();
-        send_devices([...devices].filter((d) => d.Index !== d.Index));
+        send_devices([...devices].filter((d) => d.Index !== e.Index));
     })
 }
 
-export default handler;
+export default buttplug_handler;
