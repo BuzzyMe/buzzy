b4115965f061ed614aa20b161acffe040338f6e9
amy <ctrlalternativedelete@gmail.com>
2022-10-24 21:51:05 +1100

    added get peer config

--- src/store/peer.ts
+++ src/store/peer.ts
@@ -4,12 +4,20 @@ import { handler } from "modules/multiplayer/peer/handler";
 import { PeerDevicesMessage, PeerMessage } from "modules/multiplayer/peer/message";
 import { JSONTools } from "modules/multiplayer/peer/tools";
 import { Peer as TPeer } from "peerjs";
+import { env } from "env/client.mjs";
 import create from "zustand";
 import useButtplugStore from "./buttplug";
 
 export interface PeerStoreState {
     peer?: TPeer,
     newPeerIfUndefined: () => TPeer | undefined
+}
+const getPeerConfig = () => {
+    const config: ConstructorParameters<typeof TPeer>[1] = {};
+    env.NEXT_PUBLIC_PEERJS_HOST?.length && (config.host = env.NEXT_PUBLIC_PEERJS_HOST);
+    env.NEXT_PUBLIC_PEERJS_PORT?.length && (config.port = Number(env.NEXT_PUBLIC_PEERJS_PORT));
+    env.NEXT_PUBLIC_PEERJS_PATH?.length && (config.path = env.NEXT_PUBLIC_PEERJS_PATH);
+    return config;
 }
 
 const usePeerStore = create<PeerStoreState>((set, get) => {
@@ -23,7 +31,7 @@ const usePeerStore = create<PeerStoreState>((set, get) => {
             const importedPeer: any = require('peerjs');
             const Peer: typeof TPeer = importedPeer.Peer;
 
-            const peer = new Peer();
+            const peer = new Peer({...getPeerConfig()});
             peer?.on("connection", (c) => {
                 c.on("data", (data) => {
                     const d = data as PeerMessage;
