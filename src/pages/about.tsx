a96cd5c9b18c138349e90297f15efd966276b1cf
Amy! <50583248+amydevs@users.noreply.github.com>
2022-10-25 22:42:18 +1100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,7 +9,7 @@ const About: NextPage = () => {
                 <div>
                     Buzzy! is a Progressive Web App for controlling IoT adult toys across the internet (using WebRTC P2P) and locally (through Bluetooth and Intiface). It also uses the Buttplug-rs library for support across a wide range of device models.<br/>
                     The point of this project is to make a teledildonics application that respects your privacy, is open-source, convenient, and Peer-to-Peer that works both online and offline.<br/>
-                    All of the controller networking is Peer-2-Peer with each client sending each other data. That means:<br/>
+                    All of the multiplayer controller networking is Peer-2-Peer with each client sending each other data. That means:<br/>
                     - Privacy as data cannot be intervened by a server. <br/>
                     - Lower latency as clients are streaming directly to each other. <br/>
                     - Compatiblity with a wide range of device models. <br/>
